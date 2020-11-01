1861a9fa8520b0e9a15ef4d06629c78c04cc9595
Luan França Vieira <luanfrancavieira@gmail.com>
2020-11-01 15:08:09 -0300

    config eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const App = () => {
-    return (
-        <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center' }}>
-            <Text>Search character Rick and Morty</Text>
-        </View>
-    );
-}
+const App = () => (
+  <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center' }}>
+    <Text>Search character Rick and Morty</Text>
+  </View>
+);
 
 export default App;
