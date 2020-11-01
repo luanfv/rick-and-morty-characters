8e7fd3e070504a1daed6ab1820308f28cf1b7487
Luan França Vieira <luanfrancavieira@gmail.com>
2020-11-01 14:39:05 -0300

    start project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+    return (
+        <View style={{ flex: 1, alignContent: 'center', justifyContent: 'center' }}>
+            <Text>Search character Rick and Morty</Text>
+        </View>
+    );
+}
+
+export default App;
